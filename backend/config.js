8a3cfb6d13c0438f8503cf3ecff154e4856938c9
Abhishek Gupta <webtaculars.ai@gmail.com>
2024-04-05 10:21:09 +0530

    add config

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
+  PORT: process.env.PORT || 3005,
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://root:test124@node-ecommerce.lan8zlt.mongodb.net/?retryWrites=true&w=majority&appName=node-ecommerce',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
