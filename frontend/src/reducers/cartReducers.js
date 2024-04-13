33b326b8e2774b6a00415f8048acb2918ac41846
Abhishek Gupta <webtaculars.ai@gmail.com>
2024-04-13 15:19:12 +0530

    Fix subtotal incorrect concatenation issue

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -7,20 +7,28 @@ function cartReducer(state = { cartItems: [], shipping: {}, payment: {} }, actio
       const product = state.cartItems.find(x => x.product === item.product);
       if (product) {
         return {
-          cartItems:
-            state.cartItems.map(x => x.product === product.product ? item : x)
+          ...state,
+          cartItems: state.cartItems.map(x =>
+            x.product === product.product
+              ? { ...x, qty: parseInt(x.qty) + parseInt(item.qty) }
+              : x
+          )
         };
+      } else {
+        return { ...state, cartItems: [...state.cartItems, { ...item, qty: parseInt(item.qty) }] };
       }
-      return { cartItems: [...state.cartItems, item] };
     case CART_REMOVE_ITEM:
-      return { cartItems: state.cartItems.filter(x => x.product !== action.payload) };
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(x => x.product !== action.payload)
+      };
     case CART_SAVE_SHIPPING:
       return { ...state, shipping: action.payload };
     case CART_SAVE_PAYMENT:
       return { ...state, payment: action.payload };
     default:
-      return state
+      return state;
   }
 }
 
-export { cartReducer }
+export default cartReducer;
